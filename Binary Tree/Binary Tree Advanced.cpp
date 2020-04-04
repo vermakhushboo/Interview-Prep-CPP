f7a60fc98f83f71b02791d4df7e6828ab644e652
Khushboo Verma <khush.vv@gmail.com>
2020-04-04 19:50:37 +0530

    Added functions to tree

--- Binary Tree/Binary Tree Advanced.cpp
+++ Binary Tree/Binary Tree Advanced.cpp
@@ -27,6 +27,14 @@ node* buildTree(){
 	return root;
 }
 
+int heightOfTree(node *root){
+	if(root == NULL)
+		return 0;
+	int leftHeight = heightOfTree(root->left);
+	int rightHeight = heightOfTree(root->right);
+	return max(leftHeight, rightHeight) + 1;
+}
+
 //print the kth level of a tree
 void printKthLevel(node *root, int k){
 	if(root == NULL){
@@ -43,4 +51,16 @@ void printKthLevel(node *root, int k){
 int main(){
 	node *root = buildTree();
 	printKthLevel(root, 2);
+	cout<<endl;
+	cout<<"Height of Tree "<<heightOfTree(root)<<endl;
 }
+
+//5 2 7 -1 -1 9 3 -1 -1 -1 1 3 -1 6 -1 -1 4 -1 -1 (Sample Input)
+
+//			5
+//		  /  \
+//		 2    1
+//		/ \  / \
+//	   7  9  3  4
+//	     /   \
+//	    3     6
